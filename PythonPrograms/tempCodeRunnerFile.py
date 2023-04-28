a1be5a371d8000ee50ef5493eca8439278999ee0
charchit-agarwal <charchit957@gmail.com>
2023-04-28 11:12:37 +0530

    OS prog 6 added

--- PythonPrograms/tempCodeRunnerFile.py
+++ PythonPrograms/tempCodeRunnerFile.py
@@ -1,3 +1,13 @@
-string = input("Enter a string")
-for word in reversed(string):
-    print(word)
+import sys
+
+random_list = ['a', 0, 2]
+
+for entry in random_list:
+    try:
+        print("The entry is",entry)
+        res = 1/int(entry)
+        break
+    except:
+        print("Oops!", sys.exc_info()[0],"occured")
+        print("Next entry\n")
+print("The reciprocal of", entry, "is", res)
