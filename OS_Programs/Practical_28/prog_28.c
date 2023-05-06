a41b80d708d3bf7feedd78a3eea7f77ac1953055
charchit-agarwal <charchit957@gmail.com>
2023-05-06 22:10:21 +0530

    OS prog 28 extra bracket removed

--- OS_Programs/Practical_28/prog_28.c
+++ OS_Programs/Practical_28/prog_28.c
@@ -1,37 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<fcntl.h>
-#include<sys/stat.h>
-#include<signal.h>
-#include<time.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
 
 void sig_hand(int); // declaration of signal handler function
 
-int main(){
-    int i=1;
+int main()
+{
+    int i = 1;
     pid_t pid;
     // register the signal handler
-    if(signal(SIGALRM,sig_hand)==SIG_ERR) 
+    if (signal(SIGALRM, sig_hand) == SIG_ERR)
         printf("Not Registered");
-    while(i<=5){ // loop to send signals after 2 seconds delay
+    while (i <= 5)
+    { // loop to send signals after 2 seconds delay
 
         i++;
-        pid=getpid();
-        sleep(2); // wait for 2 seconds
-        kill(pid,SIGALRM); // send the signal
+        pid = getpid();
+        sleep(2);           // wait for 2 seconds
+        kill(pid, SIGALRM); // send the signal
     }
     return 0;
 }
 
 void sig_hand(int sig)
 {
-    struct tm *t; // structure to hold the broken-down time
-    time_t tt; // variable to hold the time in seconds
-    if(sig==SIGALRM) // check if the received signal is SIGALRM{
-        tt=time(NULL); // get the current time in seconds since epoch
-        t=localtime(&tt); // convert the time to broken-down time
-        printf("%d\n",t->tm_sec); // print the seconds field of the time
-    }
+    struct tm *t;              // structure to hold the broken-down time
+    time_t tt;                 // variable to hold the time in seconds
+    if (sig == SIGALRM)        // check if the received signal is SIGALRM{
+        tt = time(NULL);       // get the current time in seconds since epoch
+    t = localtime(&tt);        // convert the time to broken-down time
+    printf("%d\n", t->tm_sec); // print the seconds field of the time
 }
