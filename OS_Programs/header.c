5f82d75519ab40938cb4e3322f618fc2cde1fe92
charchit-agarwal <charchit957@gmail.com>
2023-05-07 18:01:08 +0530

    OS header file added

---
+++ OS_Programs/header.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include<pwd.h>
+#include<error.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/utsname.h>
+#include <sys/resource.h>
+#include <sys/types.h>
